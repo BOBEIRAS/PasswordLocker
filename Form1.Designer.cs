edbb4003fa8418873c7a7d2d9f76df1ffeeef247
BOBEIRAS <lapaa3197@gmail.com>
2025-04-26 01:29:23 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace o_Mini_Cofre_de_Palavras_Passe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Userlbl = new System.Windows.Forms.Label();
+            this.sitelbl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Savebtn = new System.Windows.Forms.Button();
+            this.Copybtn = new System.Windows.Forms.Button();
+            this.Sitetxt = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.lstContas = new System.Windows.Forms.DataGridView();
+            this.Sitecoluna = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Usercoluna = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Passcoluna = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtpesquisar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.lstContas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Userlbl
+            // 
+            this.Userlbl.AutoSize = true;
+            this.Userlbl.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Userlbl.Location = new System.Drawing.Point(22, 57);
+            this.Userlbl.Name = "Userlbl";
+            this.Userlbl.Size = new System.Drawing.Size(61, 13);
+            this.Userlbl.TabIndex = 0;
+            this.Userlbl.Text = "Username :";
+            // 
+            // sitelbl
+            // 
+            this.sitelbl.AutoSize = true;
+            this.sitelbl.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sitelbl.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sitelbl.Location = new System.Drawing.Point(37, 17);
+            this.sitelbl.Name = "sitelbl";
+            this.sitelbl.Size = new System.Drawing.Size(32, 13);
+            this.sitelbl.TabIndex = 1;
+            this.sitelbl.Text = "Site :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(22, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password :";
+            // 
+            // Savebtn
+            // 
+            this.Savebtn.BackColor = System.Drawing.Color.Black;
+            this.Savebtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Savebtn.Location = new System.Drawing.Point(25, 170);
+            this.Savebtn.Name = "Savebtn";
+            this.Savebtn.Size = new System.Drawing.Size(75, 23);
+            this.Savebtn.TabIndex = 3;
+            this.Savebtn.Text = "Save";
+            this.Savebtn.UseVisualStyleBackColor = false;
+            this.Savebtn.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Copybtn
+            // 
+            this.Copybtn.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Copybtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Copybtn.Location = new System.Drawing.Point(204, 170);
+            this.Copybtn.Name = "Copybtn";
+            this.Copybtn.Size = new System.Drawing.Size(65, 23);
+            this.Copybtn.TabIndex = 4;
+            this.Copybtn.Text = "Copy";
+            this.Copybtn.UseVisualStyleBackColor = false;
+            this.Copybtn.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Sitetxt
+            // 
+            this.Sitetxt.Location = new System.Drawing.Point(89, 14);
+            this.Sitetxt.Name = "Sitetxt";
+            this.Sitetxt.Size = new System.Drawing.Size(180, 20);
+            this.Sitetxt.TabIndex = 6;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(89, 50);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(180, 20);
+            this.txtUsername.TabIndex = 7;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(89, 95);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(180, 20);
+            this.txtPass.TabIndex = 8;
+            // 
+            // lstContas
+            // 
+            this.lstContas.AllowUserToAddRows = false;
+            this.lstContas.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lstContas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.lstContas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Sitecoluna,
+            this.Usercoluna,
+            this.Passcoluna});
+            this.lstContas.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lstContas.Location = new System.Drawing.Point(-41, 219);
+            this.lstContas.Name = "lstContas";
+            this.lstContas.Size = new System.Drawing.Size(433, 194);
+            this.lstContas.TabIndex = 9;
+            this.lstContas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPasswords_CellClick);
+            this.lstContas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.lstContas_CellContentClick);
+            // 
+            // Sitecoluna
+            // 
+            this.Sitecoluna.HeaderText = "Site";
+            this.Sitecoluna.Name = "Sitecoluna";
+            // 
+            // Usercoluna
+            // 
+            this.Usercoluna.HeaderText = "Username";
+            this.Usercoluna.Name = "Usercoluna";
+            // 
+            // Passcoluna
+            // 
+            this.Passcoluna.HeaderText = "Password";
+            this.Passcoluna.Name = "Passcoluna";
+            // 
+            // txtpesquisar
+            // 
+            this.txtpesquisar.Location = new System.Drawing.Point(89, 136);
+            this.txtpesquisar.Name = "txtpesquisar";
+            this.txtpesquisar.Size = new System.Drawing.Size(180, 20);
+            this.txtpesquisar.TabIndex = 11;
+            this.txtpesquisar.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(34, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Search :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(298, 407);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpesquisar);
+            this.Controls.Add(this.lstContas);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.Sitetxt);
+            this.Controls.Add(this.Copybtn);
+            this.Controls.Add(this.Savebtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sitelbl);
+            this.Controls.Add(this.Userlbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PasswordVault";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.lstContas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Userlbl;
+        private System.Windows.Forms.Label sitelbl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Savebtn;
+        private System.Windows.Forms.Button Copybtn;
+        private System.Windows.Forms.TextBox Sitetxt;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.DataGridView lstContas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sitecoluna;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Usercoluna;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Passcoluna;
+        private System.Windows.Forms.TextBox txtpesquisar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿// Form1.cs
+using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace o_Mini_Cofre_de_Palavras_Passe
+{
+    public partial class Form1 : Form
+    {
+        private string chaveCorreta = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            CustomizeDesign();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Mensagem de boas-vindas e verificação da existência da chave mestra
+            try
+            {
+                if (File.Exists("key.txt"))
+                {
+                    chaveCorreta = File.ReadAllText("key.txt").Trim();
+
+                    if (string.IsNullOrWhiteSpace(chaveCorreta))
+                    {
+                        MessageBox.Show("The file 'key.txt' is empty. Please enter a new key.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        PromptForNewKey();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("The file 'key.txt' was not found! A new key will be created.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    PromptForNewKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error reading the file 'key.txt': {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // Carregar dados anteriores do ficheiro contas.txt
+            if (File.Exists("contas.txt"))
+            {
+                string[] linhas = File.ReadAllLines("contas.txt");
+
+                foreach (string linha in linhas)
+                {
+                    string[] partes = linha.Split(';');
+                    if (partes.Length == 3)
+                    {
+                        lstContas.Rows.Add(partes[0], partes[1], partes[2]);
+                    }
+                }
+            }
+        }
+
+        // Criação e guarda da nova chave mestra
+        private void PromptForNewKey()
+        {
+            string newKey = Microsoft.VisualBasic.Interaction.InputBox("Enter a new key:", "New Key");
+
+            if (!string.IsNullOrWhiteSpace(newKey))
+            {
+                chaveCorreta = newKey.Trim();
+                File.WriteAllText("key.txt", chaveCorreta);
+                MessageBox.Show("Key saved successfully.");
+            }
+            else
+            {
+                MessageBox.Show("No key was entered. Some features may not work.");
+            }
+        }
+
+        // Ação do botão Guardar
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string site = Sitetxt.Text;
+            string username = txtUsername.Text;
+            string password = EncryptBase64(txtPass.Text);
+
+            if (!string.IsNullOrEmpty(site) && !string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                lstContas.Rows.Add(site, username, password);
+
+                // Guardar os dados da conta num ficheiro .txt
+                string fileName = $"{site}.txt";
+                string content = $"Site: {site}\nUsername: {username}\nPassword (Encrypted): {password}\nPassword (Decrypted): {txtPass.Text}";
+
+                try
+                {
+                    File.WriteAllText(fileName, content);
+                    MessageBox.Show($"Saved to file: {fileName}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // Acrescentar linha ao ficheiro geral contas.txt
+                    File.AppendAllText("contas.txt", $"{site};{username};{password}{Environment.NewLine}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error saving to file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                Sitetxt.Clear();
+                txtUsername.Clear();
+                txtPass.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all fields!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // Ação do botão Copiar
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (lstContas.CurrentRow != null)
+            {
+                string copiedPassword = lstContas.CurrentRow.Cells[2].Value.ToString();
+                Clipboard.SetText(copiedPassword);
+            }
+        }
+
+        // Filtro de pesquisa em tempo real
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string search = txtpesquisar.Text.ToLower();
+            foreach (DataGridViewRow row in lstContas.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    bool visible = row.Cells[0].Value.ToString().ToLower().Contains(search) ||
+                                   row.Cells[1].Value.ToString().ToLower().Contains(search);
+                    row.Visible = visible;
+                }
+            }
+        }
+
+        // Mostrar password desencriptada ao clicar numa célula
+        private void dgvPasswords_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                string encryptedPassword = lstContas.Rows[e.RowIndex].Cells[2].Value.ToString();
+                string decryptedPassword = DecryptBase64(encryptedPassword);
+                MessageBox.Show($"Password: {decryptedPassword}", "Decrypted Password");
+            }
+        }
+
+        // Encriptação de texto em Base64
+        private string EncryptBase64(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            return Convert.ToBase64String(bytes);
+        }
+
+        // Desencriptação de Base64 para texto normal
+        private string DecryptBase64(string base64Text)
+        {
+            byte[] bytes = Convert.FromBase64String(base64Text);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        // Estilo visual escuro da aplicação
+        private void CustomizeDesign()
+        {
+            this.BackColor = Color.FromArgb(30, 30, 30);
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is Button btn)
+                {
+                    btn.BackColor = Color.FromArgb(45, 45, 45);
+                    btn.ForeColor = Color.White;
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.FlatAppearance.BorderSize = 0;
+                }
+                else if (ctrl is TextBox txt)
+                {
+                    txt.BackColor = Color.FromArgb(50, 50, 50);
+                    txt.ForeColor = Color.White;
+                    txt.BorderStyle = BorderStyle.FixedSingle;
+                }
+                else if (ctrl is DataGridView dgv)
+                {
+                    dgv.BackgroundColor = Color.FromArgb(40, 40, 40);
+                    dgv.DefaultCellStyle.BackColor = Color.FromArgb(50, 50, 50);
+                    dgv.DefaultCellStyle.ForeColor = Color.White;
+                    dgv.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(60, 60, 60);
+                    dgv.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+                    dgv.EnableHeadersVisualStyles = false;
+                }
+            }
+        }
+
+        private void lstContas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
